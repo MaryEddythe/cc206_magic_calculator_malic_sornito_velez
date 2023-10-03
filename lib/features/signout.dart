e159e35f13f5e06a8bb33c16e7e36d5706d16d02
Kyle G. Velez <121173363+kaidencore@users.noreply.github.com>
2023-10-03 12:42:22 +0000

    velez signout

---
+++ lib/features/signout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Signout extends StatelessWidget {
+  const Signout({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+	body:  Column(
+	children: [
+
+
+        ]
+      ),
+    );
+  }
+}
