cf01c8e9889be1cafef05da9ac05c815127036d4
Josef Alexander Malic <124750288+alexmalic62@users.noreply.github.com>
2023-10-03 13:03:41 +0000

    malic home

---
+++ lib/features/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+	body:  Column(
+	children: [
+
+
+        ]
+      ),
+    );
+  }
+}
