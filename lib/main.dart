32974297a237df184426935426b3337dc0c992bc
Mary Eddythe Sornito <sornito.maryeddythe@gmail.com>
2023-10-03 21:24:42 +0800

    velez main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Sign Out',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.Green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const Signout(),
