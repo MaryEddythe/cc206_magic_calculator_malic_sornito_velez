d72262d7de58df2a243ba4d5f5e2f866d23c19d7
Mary Eddythe Sornito <sornito.maryeddythe@gmail.com>
2023-10-03 19:49:43 +0800

    sign in sornito

---
+++ lib/features/signin.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  const Signin({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+	body:  Column(
+	children: [
+
+
+        ]
+      ),
+    );
+  }
+}
