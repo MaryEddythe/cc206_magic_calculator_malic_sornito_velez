4cc34c484ba8b174e9be00412d592873107caf14
Mary Eddythe Sornito <sornito.maryeddythe@gmail.com>
2023-10-05 18:04:48 +0800

    sign in widgets sornito

--- lib/features/signin.dart
+++ lib/features/signin.dart
@@ -2,18 +2,50 @@ import 'package:flutter/material.dart';
 
 class Signin extends StatelessWidget {
   const Signin({super.key});
-
-  // This widget is the root of your application.
+//sample sign in rough draft lang//
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-	body:  
-  Column(
-	children: [
-    Text("Sign in Calulator")
-
-        ],
-  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 225,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 225,
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              label: Text('Sign In'),
+              icon: Icon(Icons.door_front_door),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
