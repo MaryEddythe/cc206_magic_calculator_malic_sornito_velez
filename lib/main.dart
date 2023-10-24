6ca41de0bc09b1fa26d8630e0d3adff6ef374f91
Kyle G. Velez <kyleg.velez@gmail.com>
2023-10-24 23:35:54 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:cc206_the_news_app_sornito_velez/features/home.dart';
+import 'package:cc206_magic_calculator_malic_sornito_velez/features/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
