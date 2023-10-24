1a69b70cdb282183aab28775d4a71b607be828e9
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-10-24 09:14:40 +0800

    change bg

--- lib/features/signin.dart
+++ lib/features/signin.dart
@@ -6,44 +6,51 @@ class Signin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 225,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/wallpaper.jpg"), // Replace with the path to your image asset
+            fit: BoxFit.cover, // You can change the fit as needed
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 225,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: 225,
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+              SizedBox(height: 20),
+              Container(
+                width: 225,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
+              SizedBox(height: 20),
+               ElevatedButton(
               onPressed: () {
-                Navigator.pop(context);
-              },
-              label: Text('Sign In'),
-              icon: Icon(Icons.door_front_door),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.deepPurple,
-                foregroundColor: Colors.white,
-              ),
-            ),
-          ],
+             Navigator.pop(context);
+               },
+               style: ElevatedButton.styleFrom(
+               backgroundColor: Color(0xFF0b4f6c),
+               foregroundColor: Colors.white,
+               ),
+               child: Text('Sign In'),
+               )
+            ],
+          ),
         ),
       ),
     );
