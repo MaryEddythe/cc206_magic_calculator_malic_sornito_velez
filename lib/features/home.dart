9db2a874f1e54a0d88a24941c4448a2b6e108dab
Josef Alexander Malic <124750288+alexmalic62@users.noreply.github.com>
2023-10-25 04:37:17 +0000

    home

---
+++ lib/features/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          CircleAvatar(
+            radius: 50,
+          ),
+          SizedBox(height: 16),
+          Text(
+            "John Dear",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Flutter Developer',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.location_on, color: Colors.grey),
+              SizedBox(width: 8.0),
+              Text(
+                'Iloilo City, Philippines',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
