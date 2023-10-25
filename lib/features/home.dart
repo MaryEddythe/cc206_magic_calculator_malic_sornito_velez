d1a0b9536d9132566ef74ea075e654f45aae915a
Kaidencore <kyleg.velez@gmail.com>
2023-10-25 11:44:10 +0800

    profile

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatelessWidget {
-  const Homescreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-	body:  Column(
-	children: [
-          Text("Welcome to Calculator")     
-
-        ]
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          CircleAvatar(
+            radius: 50,
+          ),
+          SizedBox(height: 16),
+          Text(
+            "John Dear",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Flutter Developer',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.location_on, color: Colors.grey),
+              SizedBox(width: 8.0),
+              Text(
+                'Iloilo City, Philippines',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
