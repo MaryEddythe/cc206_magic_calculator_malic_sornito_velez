10e298bcd62762885eb2b1afd0487bd85bd1cc33
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-10-26 20:46:50 +0800

    modify

--- lib/features/signin.dart
+++ lib/features/signin.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 class Signin extends StatelessWidget {
   const Signin({Key? key}) : super(key: key);
 
