80c35c27b21bb1e34a6cb454b7a0614f093f1187
alexmalic62 <josefalex612@gmail.com>
2023-11-29 23:30:58 +0800

    redirections

--- lib/redirections/soc_med.dart
+++ lib/redirections/soc_med.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SocialMediaScreen extends StatelessWidget {
   @override
@@ -7,11 +8,46 @@ class SocialMediaScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Social Medias'),
       ),
-      body: Center(
-        child: Text(
-          'Welcome to the Social Media Screen!',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Social Media Screen!',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            buildSocialMediaItem(
+              icon: FontAwesomeIcons.instagram,
+              label: '@moleclec',
+            ),
+            buildSocialMediaItem(
+              icon: FontAwesomeIcons.facebook,
+              label: 'Alexander Malic',
+            ),
+            buildSocialMediaItem(
+              icon: Icons.mail,
+              label: 'josefalex612@gmail.com',
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget buildSocialMediaItem({required IconData icon, required String label}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Icon(icon, size: 30),
+          SizedBox(width: 16),
+          Text(
+            label,
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
       ),
     );
   }
