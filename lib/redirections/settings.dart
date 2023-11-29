7d06d90c5ffb9fe55235ab8a747302df01daea64
alexmalic62 <josefalex612@gmail.com>
2023-11-29 23:42:24 +0800

    redirections

--- lib/redirections/settings.dart
+++ lib/redirections/settings.dart
@@ -7,10 +7,54 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text(
-          'Welcome to the Settings Screen!',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildSettingItem(
+              label: 'Notifications',
+              icon: Icons.notifications,
+            ),
+            buildSettingItem(
+              label: 'Account',
+              icon: Icons.account_circle,
+            ),
+            buildSettingItem(
+              label: 'Privacy',
+              icon: Icons.lock,
+            ),
+            // Add more settings items as needed
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSettingItem({required String label, required IconData icon}) {
+    return InkWell(
+      onTap: () {
+        // Handle setting item tap
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              size: 30,
+              color: Colors.black,
+            ),
+            SizedBox(width: 16),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ],
         ),
       ),
     );
