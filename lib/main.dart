b9977b787532d06f2c2a73b67b0ef68c96d166f2
alexmalic62 <josefalex612@gmail.com>
2023-11-29 23:52:47 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Homescreen', 
-      theme: ThemeData( 
+      theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
