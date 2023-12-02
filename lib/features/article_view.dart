5a2bfb906be29eee4a9ffd2880d8ffd74855c524
Kyle G. Velez <kyleg.velez@gmail.com>
2023-12-03 03:34:45 +0800

    home

---
+++ lib/features/article_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+// ignore: must_be_immutable
+class ArticleView extends StatefulWidget {
+  String blogUrl;
+  ArticleView({required this.blogUrl});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Edithor.ial",
+                style: TextStyle(
+                    fontFamily: 'Lora',
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: Color(0xFF001747),
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        body: Container(
+          child: WebView(
+            initialUrl: widget.blogUrl,
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        ));
+  }
+}
