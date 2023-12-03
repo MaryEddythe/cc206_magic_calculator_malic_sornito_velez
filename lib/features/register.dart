999e5b667ab038b8bb1a9cf7ab2cfb18f90f1613
Kyle G. Velez <kyleg.velez@gmail.com>
2023-12-04 03:16:43 +0800

    register

---
+++ lib/features/register.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  RegisterState createState() => RegisterState();
+}
+
+class RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Create Account',
+            style: TextStyle(
+              fontSize: 16,
+              fontFamily: 'Lora',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF001747),
+        ),
+        body: Center(
+          child: const RegisterContent(),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterContent extends StatelessWidget {
+  const RegisterContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Create Account',
+          style: TextStyle(
+            fontSize: 20,
+            color: Color(0xFFFF001747),
+            fontFamily: "Poppins",
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    );
+  }
+}
